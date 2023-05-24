70a536f893a58b0e76410dc5b447ba80fbdc2e51
kelly-jsx <kelly-jsx@proton.me>
2023-05-24 14:43:47 +0200

    title, prettier

---
+++ app/components/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className="w-screen h-screen bg-base">
+      <h1 className="text-pink">Kelly-JSX</h1>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from './components/Hero'
+
 export default function Home() {
   return (
     <main className="w-full h-full bg-base">
-      <h1 className=" text-pink">Kelly-JSX</h1>
+      <Hero />
     </main>
   )
 }
